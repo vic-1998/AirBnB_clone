088f731c76930dd0af73bc3431f8d15f51abc176
David Alzate <illker@hotmail.com>
2021-02-12 21:33:19 +0000

    changes :soccer:

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,8 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """string representation"""
@@ -36,6 +38,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
