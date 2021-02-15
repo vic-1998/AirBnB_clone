d0644461f442571d3b15cd964bac3f452ca713ad
vic-1998 <victor.mplaza98@gmail.com>
2021-02-15 17:56:32 +0000

    Add file Place.

---
+++ models/place.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""Contains class Place"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """The Placeclass"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
