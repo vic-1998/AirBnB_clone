936afeb147fa2d0ade3419ba0723af82989ec7a9
David Alzate <illker@hotmail.com>
2021-02-15 17:29:17 +0000

    User creation :sunglasses:

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Contains Class BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """The User class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the User Class"""
+        super().__init__(*args, **kwargs)
