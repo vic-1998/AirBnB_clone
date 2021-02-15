ca468624bb8ca44c7a0df9abc981a8a704df9633
David Alzate <illker@hotmail.com>
2021-02-15 17:37:11 +0000

    update cat atri :smile_cat:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,16 @@ Class FileStorage
 """
 
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import json
 
-atri = {"BaseModel": BaseModel}
+atri = {"BaseModel": BaseModel, "City": City, "State": State,
+        "Amenity": Amenity, "Place": Place, "Review": Review}
 
 
 class FileStorage():
